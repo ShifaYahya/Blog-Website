553ae036290ce8704fb8bfb9b859ed766198c8d4
Shifa <shifayahya327@gmail.com>
2024-06-26 15:39:28 +0530

    fixed some dependency array errors

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,46 +1,53 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import {addDoc, collection} from 'firebase/firestore'
-import {db, auth} from '../firebase-config';
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { addDoc, collection } from 'firebase/firestore';
+import { db, auth } from '../firebase-config';
 import { useNavigate } from 'react-router-dom';
 
+const CreatePost = ({ isAuth }) => {
+  const [title, setTitle] = useState('');
+  const [postText, setPostText] = useState('');
 
+  const postsCollectionRef = collection(db, 'posts'); // second argument here is the name of the collection you want to reference
+  let navigate = useNavigate();
 
-const CreatPost = ({isAuth}) => {
-const [title, setTitle] = useState("")
-const [postText, setPostText] = useState("")
+  const createPost = async () => {
+    await addDoc(postsCollectionRef, {
+      title,
+      postText,
+      author: { name: auth.currentUser.displayName, id: auth.currentUser.uid },
+    });
+    navigate('/');
+  };
 
-const postsCollectionRef = collection(db, 'posts') //second argument here is name of the collection you want to reference
-let navigate = useNavigate();
-
-const createPost = async ()=>{
-  await addDoc(postsCollectionRef, {title, postText, author: {name: auth.currentUser.displayName , id: auth.currentUser.uid}})
-  navigate("/")
-}
-
-useEffect(()=>{
-if(!isAuth){
-  navigate("/login") //if we're not authenticated we're redirected tot the login page
-}
-}, []) //useEffect here to check once this comoonent is refreshed or shown
-
+  useEffect(() => {
+    if (!isAuth) {
+      navigate('/login'); // if we're not authenticated, we're redirected to the login page
+    }
+  }, [isAuth, navigate]); // Add isAuth and navigate to the dependency array
 
   return (
-    <div className='createPostPage'>
-    <div className='cpContainer'>
-      <h1>Create A Post</h1>
-      <div className="inputGp">
-        <label> TItle:</label>
-        <input placeholder='Enter A Title..' onChange={(event)=> setTitle(event.target.value)}></input>
+    <div className="createPostPage">
+      <div className="cpContainer">
+        <h1>Create A Post</h1>
+        <div className="inputGp">
+          <label>Title:</label>
+          <input
+            placeholder="Enter A Title.."
+            onChange={(event) => setTitle(event.target.value)}
+          ></input>
         </div>
-      <div className="inputGp">
-      <label> Post:</label>
-      <textarea placeholder='Write Your Post...' onChange={(event)=>setPostText(event.target.value)}></textarea>
-      </div> 
-      <button onClick={createPost}>Submit Post</button>
+        <div className="inputGp">
+          <label>Post:</label>
+          <textarea
+            placeholder="Write Your Post..."
+            onChange={(event) => setPostText(event.target.value)}
+          ></textarea>
+        </div>
+        <button onClick={createPost}>Submit Post</button>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default CreatPost
+export default CreatePost;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,69 +1,65 @@
-import React from 'react'
-import {useState, useEffect} from 'react'
-import { getDocs } from 'firebase/firestore'
-import { collection, deleteDoc, doc } from 'firebase/firestore'
-import { db, auth } from '../firebase-config'
+import React, { useState, useEffect } from 'react';
+import { getDocs, collection, deleteDoc, doc } from 'firebase/firestore';
+import { db, auth } from '../firebase-config';
 
-export const Home = ({isAuth}) => {
+export const Home = ({ isAuth }) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const [postList, setPostList] = useState([]);
+  const postCollectionRef = collection(db, 'posts');
 
- const [isHovered, setIsHovered] = useState(false);  //state for delete hover
-  const handleMouseEnter = () => { //can also specify this in the onclick itself for more simplicity 
+  const handleMouseEnter = () => {
     setIsHovered(true);
   };
+  
   const handleMouseLeave = () => {
     setIsHovered(false);
   };
 
-const [postList, setPostList] = useState([])
-const postCollectionRef = collection(db, 'posts')
-
-const deletePost = async (id) => {
+  const deletePost = async (id) => {
     try {
       const postDoc = doc(db, 'posts', id);
       await deleteDoc(postDoc);
-      // After deletion, refresh the post list
       getPosts();
     } catch (error) {
       console.error('Error deleting post:', error);
     }
   };
-//takes and id as a a parameter
-    //this is to specify exactly which doc you want //this id value will change depending on which post/button you're on
-    
 
-const getPosts = async () => {
+  const getPosts = async () => {
     const data = await getDocs(postCollectionRef);
     setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
   };
-useEffect(() => {
-    
 
+  useEffect(() => {
     getPosts();
-  }, [deletePost]); // forgot to fix something in the video. In order to prevent infinite reads and in order for the delete to refresh after a post is deleted, you need to change the useffect in the home page to the following:
+  }, [getPosts]);
 
- 
-
-
-  return ( 
+  return (
     <div className='homePage'>
-       {postList.map((post)=> {
+      {postList.map((post) => {
         return (
-            <div className='post'>
-                <div className='postHeader'>
-                    <div className='title'>
-                      <h1>{post.title}</h1>  </div>
-                      <div className='deletePost'>
-                        {isAuth && post.author.id === auth.currentUser.uid && (<button  onClick={() =>{deletePost(post.id)}} onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}><img src='https://cdn-icons-png.flaticon.com/512/5037/5037730.png
-      '  height={30} width={30}  alt='trashcan/delete'></img> {isHovered? <div className='delete-hover'>Delete</div> : null}</button>) }
-                      </div>
-                </div>
-                <div className='postTextContainer'>{post.postText}</div>
-                <h3>@{post.author.name}</h3>
+          <div className='post' key={post.id}>
+            <div className='postHeader'>
+              <div className='title'>
+                <h1>{post.title}</h1>
+              </div>
+              <div className='deletePost'>
+                {isAuth && post.author.id === auth.currentUser.uid && (
+                  <button 
+                    onClick={() => { deletePost(post.id) }} 
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}>
+                    <img src='https://cdn-icons-png.flaticon.com/512/5037/5037730.png' height={30} width={30} alt='trashcan/delete'></img>
+                    {isHovered ? <div className='delete-hover'>Delete</div> : null}
+                  </button>
+                )}
+              </div>
             </div>
-        )
-       })}
-    </div> 
-  )
+            <div className='postTextContainer'>{post.postText}</div>
+            <h3>@{post.author.name}</h3>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
-
